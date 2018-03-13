31f76649d9ba8b97ce729c3248819c59762b3383
Seanforfun <32276204+Seanforfun@users.noreply.github.com>
2018-03-13 14:36:52 -0400

    [Function add]
    1.Practice easyUi.

---
+++ 2018-03-13easyUi/src/ca/mcmaster/servlet/MyTestServlet.java
@@ -0,0 +1,24 @@
+package ca.mcmaster.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyTestServlet
+ */
+@SuppressWarnings("serial")
+public class MyTestServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.doPost(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("get!");
+		response.getWriter().write("returned value");
+	}
+
+}
